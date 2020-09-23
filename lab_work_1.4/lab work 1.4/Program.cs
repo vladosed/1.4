08b502eb383f74eb52624b6baa70940a0e9acd53
Vlad Sodolevskiy <v.sodolevskiy@gmail.com>
2020-09-23 21:02:18 +0300

    Added lab work

---
+++ lab_work_1.4/lab work 1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_work_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
